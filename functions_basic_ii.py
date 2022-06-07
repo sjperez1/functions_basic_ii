425484942c8eb25edc13cefe0aca380145dcbc73
sjperez1 <sarahpz479@gmail.com>
2022-06-07 08:10:32 -0600

    adjusted naming

--- functions_basic_ii.py
+++ functions_basic_ii.py
@@ -9,35 +9,35 @@ print(countdown(6))
 
 # 2. Print and Return
 two_numbers = [6, 4]
-def print_and_return(numberlist):
-    print(numberlist[0])
-    return numberlist[1]
+def print_and_return(number_list):
+    print(number_list[0])
+    return number_list[1]
 print(print_and_return(two_numbers))
 
 # 3. First Plus Length
 first_plus_length_list = [4,2,8,9,3,7,5]
-def first_plus_length(firstlengthlist):
-    return firstlengthlist[0] + len(firstlengthlist)
+def first_plus_length(first_length_list):
+    return first_length_list[0] + len(first_length_list)
 print(first_plus_length(first_plus_length_list))
 
 # 4. Values Greater than Second
 original_list = [7,3,4,8,1,6,2]
 new_list = []
-def values_greater_than_second(greaterthansecondlist):
-    if len(greaterthansecondlist) < 2:
+def values_greater_than_second(greater_than_second_list):
+    if len(greater_than_second_list) < 2:
         return False
     else:
-        for i in range(0,len(greaterthansecondlist)-1):
-            if greaterthansecondlist[i] > greaterthansecondlist[1]:
-                new_list.append(greaterthansecondlist[i])
+        for i in range(0,len(greater_than_second_list)-1):
+            if greater_than_second_list[i] > greater_than_second_list[1]:
+                new_list.append(greater_than_second_list[i])
     print(len(new_list))
     return new_list
 print(values_greater_than_second(original_list))
 
 # 5. This Length, That Value
-lengthvaluelist = [ ] 
+length_value_list = [ ] 
 def length_and_value(size,value):
     for i in range(0, size):
-        lengthvaluelist.append(value)
-    return lengthvaluelist
+        length_value_list.append(value)
+    return length_value_list
 print(length_and_value(5,8))
